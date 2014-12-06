279db125a02fafc49195cbb3785169f968fadadf
Cole Murray <crzdcole@gmail.com>
2014-12-06 00:54:21 -0800

    Fixed all memory leaks. Fixed Error with RSA_encrypt giving different values
    Need to sign hashed challenge before sending back

--- client.c
+++ client.c
@@ -22,24 +22,29 @@ SSL_CTX*  initCTX(){
 BIO* connectSSL(SSL_CTX *ctx, char *formattedServPort){
 	BIO *bio;
 	SSL *ssl;
-
+/*
 	bio = BIO_new_ssl_connect(ctx);
 	if (bio == NULL ){
+		BIO_free_all(bio);
+		SSL_CTX_free(ctx);
 		printf("Error setting up bio \n");
 		exit(0);
 	}
-
+*/
 	
-	BIO_get_ssl (bio, &ssl);
+/*	BIO_get_ssl (bio, &ssl);
 
 	if (!ssl){
+		BIO_free_all(bio);
+		SSL_CTX_free(ctx);
 		printf( "can't locat sslpointer \n" );
-	}
+		exit(0);
+	} */
 	// set read/write operations to only return after the handshake
 	// and successful completion
-	SSL_set_mode(ssl,SSL_MODE_AUTO_RETRY);
-	
-	//bio = BIO_new_connect ("127.0.0.1:5555"); //set ipaddress:port
+//	SSL_set_mode(ssl,SSL_MODE_AUTO_RETRY);
+	//BIO_set_conn_hostname(bio,formattedServPort);
+
 	bio = BIO_new_connect (formattedServPort);
 	
 	/* attempts connection */
@@ -63,46 +68,74 @@ BIO* connectSSL(SSL_CTX *ctx, char *formattedServPort){
 unsigned char * allocateOutputBuf (RSA *rsa_public_key){
 	int maxSize = RSA_size(rsa_public_key);
 	printf ("Max size is : %d \n" ,maxSize);
-	unsigned char *rsaSizeBuf = (unsigned char * ) malloc (maxSize);
+	unsigned char *rsaSizeBuf = (unsigned char * ) malloc (maxSize+1);
+	return rsaSizeBuf;
 }
-unsigned char* encryptChallenge (char *challenge){
+unsigned char* encryptChallenge (unsigned char *challenge, int* messageLength){
 	BIO *keyFile = BIO_new_file("./pubkey.pem", "r" );
 	if (keyFile == NULL){
 		printf ( "Error with bio object in encrypt");
 	}
-	RSA *rsa_public_key = RSA_new();
-        rsa_public_key = PEM_read_bio_RSA_PUBKEY(keyFile, NULL,NULL,NULL);
+	RSA *rsa_public_key = PEM_read_bio_RSA_PUBKEY(keyFile, NULL,NULL,NULL);
 	if (rsa_public_key == NULL)
 	{
 		printf( "Error wit public key" );
 		exit(0);
 	}
 
-	int pubSize = RSA_size(rsa_public_key);	
 
 	unsigned char * output = allocateOutputBuf( rsa_public_key );
 
 	// encrypt challenge with public key, loads into outputBuffer
 	// challengeLen must be < RSA_size(rsa_ -11 for RSA_PKCS1_Padding
-
-	if (RSA_public_encrypt(strlen(challenge),(unsigned char*) challenge, output, rsa_public_key,
-					RSA_PKCS1_PADDING) < 0 ) {
+	printf ("Challenge is size of: %lu \n", strlen(challenge));
+	int sizeEncrypted = RSA_public_encrypt(strlen(challenge),
+			      (unsigned char*) challenge,
+			      output, rsa_public_key,
+			      RSA_PKCS1_PADDING);
+	if (sizeEncrypted <= 0 ) {
 		printf ("Error encrypting challenge");
 		exit(0);
 	}
+	*messageLength = sizeEncrypted;
+	RSA_free(rsa_public_key);
+	BIO_free_all(keyFile);
 	return output;
-
-
 }
 
 
-void sendToServer ( BIO* bio, unsigned char *message){
-	if(BIO_write ( bio, message, strlen (message)) <= 0 ){
+void sendToServer ( BIO* bio, unsigned char *message, int* messageLength){
+	printf("Writing message:size: %d \n",*messageLength);
+	if(BIO_write ( bio, message, *messageLength) <= 0 ){
 		printf ("Error writing to server");
 	}
 
 }
 
+int compareHashFromServer (BIO* bio, unsigned char* challenge){
+	unsigned char recvBuf[20]; //SHA-1 20 bits
+	unsigned char hashedChallenge[20];
+	
+	printf("Challenge is size: %lu, reading : %s \n",strlen(challenge),challenge);
+	//Hash the challenge
+	SHA1((const unsigned char*)challenge,strlen(challenge),hashedChallenge);
+	int bytesReceived = 0;
+
+	bytesReceived = BIO_read(bio,recvBuf,sizeof recvBuf);
+	if (bytesReceived <= 0 ){
+		printf ( "Error reading hashed challenge" );
+		exit(0);
+	}
+	printf ("read from server: %d bytes \n", bytesReceived);
+
+	if (memcmp( hashedChallenge, recvBuf, sizeof recvBuf ) == 0){
+		printf ( "Congrats they match \n ");
+		return 1;
+	}
+	
+
+
+}	
 char* formatServerPort(char *hostname, char *portnum ){
 	char *formattedServerPort =  (char*) malloc (strlen(hostname) + strlen(portnum) + 2); // \0' and :
 	strcpy (formattedServerPort, hostname);
@@ -124,7 +157,7 @@ int main (int count, char *args[]){
 	}
 	
 	//initialize SSL library
-	OpenSSL_add_all_algorithms();
+//	OpenSSL_add_all_algorithms();
 	hostname = args[1];
 	portnum = args[2];
 	filename = args[3];
@@ -133,13 +166,25 @@ int main (int count, char *args[]){
 	char *formattedServerPort = formatServerPort(hostname,portnum);
 
 	BIO * serverConnection = connectSSL(ctx,formattedServerPort);
-
-	unsigned char *challenge = "Very Long string lol";
-	unsigned char* encryptedChallenge = encryptChallenge(challenge);
+	
+	int* messageLength = (int*) malloc (sizeof (int));
+	unsigned char *challenge = "Random Challenge";
+	unsigned char* encryptedChallenge = encryptChallenge(challenge, messageLength);
+	printf("Message Length: %d \n",*messageLength);	
 	// Send Challenge to server
-	sendToServer (serverConnection,encryptedChallenge );
+	sendToServer (serverConnection,encryptedChallenge, messageLength );
+	// Receive hash from server
+	compareHashFromServer(serverConnection, challenge);
 
 	
+	free(encryptedChallenge);
+	free (messageLength);		
+	BIO_free_all(serverConnection);		
+	free(formattedServerPort);
+	SSL_CTX_free(ctx);
+	EVP_cleanup();
+	sk_SSL_COMP_free(SSL_COMP_get_compression_methods());
+	CRYPTO_cleanup_all_ex_data();
 	return 0;
 }
 	
--- server.c
+++ server.c
@@ -2,18 +2,35 @@
 #include <openssl/ssl.h>
 #include <openssl/bio.h>
 
+void cleanUpOpenSSL(){
+	ENGINE_cleanup();
+	CONF_modules_unload(1);
+	ERR_remove_state(0);
+	ERR_free_strings();
+	EVP_cleanup();
+	sk_SSL_COMP_free(SSL_COMP_get_compression_methods());
+	CRYPTO_cleanup_all_ex_data();
+	
+}
+
 BIO * waitAndAcceptConnection(char* portnum){
 	BIO *connection;
 	connection = BIO_new_accept(portnum); 
 
+	if (connection == NULL){
+		printf ("Error setting up connection\n");
+	}
 	/* setup comm_fd */
 	if(BIO_do_accept(connection) <=0){
 		printf ("Error setting up comm_fd  \n");
+		BIO_free_all(connection);
+		cleanUpOpenSSL();	
 		exit (0);
 	}	
 	
 	if (BIO_do_accept(connection) <=0){
 		printf ( "Error accepting connection \n" );
+		BIO_free_all(connection);
 		exit(0);
 	}
 	printf( "Connection Established on port: %s \n", portnum );
@@ -25,6 +42,7 @@ RSA* getPublicKey (){
 	RSA *rsaPublicKey = PEM_read_bio_RSA_PUBKEY(pubFile,NULL,NULL,NULL);
 	int rsaSize = RSA_size(rsaPublicKey);
 	printf ("Read keysize of : %d \n", rsaSize);
+	BIO_free_all(pubFile);
 	return rsaPublicKey;
 }
 
@@ -32,18 +50,18 @@ RSA* getPrivateKey(){
 	BIO *privFile = BIO_new_file("./privkey.pem","r");
 	RSA *rsaPrivKey = PEM_read_bio_RSAPrivateKey (privFile,NULL,NULL,NULL);
 	int rsaSize = RSA_size(rsaPrivKey);
+	BIO_free_all(privFile);
 	return rsaPrivKey;	
 }
 
 
-BIO *keyExchangeWithClient(BIO* connection){
+unsigned char* getChallengeFromClient(BIO* connection){
 	RSA *rsaPublicKey = getPublicKey();
 	int rsaSize = RSA_size(rsaPublicKey);
 	printf ("Read keysize of : %d \n", rsaSize);
 	int bytesReceived;
-	char recvBuf[rsaSize];
+	unsigned char recvBuf[rsaSize];
 
-	printf ("Malloc'd %d bytes", sizeof (recvBuf) );
 	bytesReceived = BIO_read(connection,recvBuf,sizeof recvBuf );
 	if (bytesReceived <= 0 ){
 		printf ("Error receiving challenge \n" );
@@ -54,16 +72,36 @@ BIO *keyExchangeWithClient(BIO* connection){
 	RSA* rsaPrivKey = getPrivateKey();
 	int rsaPrivSize = RSA_size(rsaPrivKey);
 	
-	char decRecvBuf[bytesReceived];
+	unsigned char* decRecvBuf = (unsigned char*) malloc (rsaSize);
 	RSA_private_decrypt(bytesReceived, recvBuf,
-			 (unsigned char*)decRecvBuf, rsaPrivKey,RSA_PKCS1_PADDING);
+			  decRecvBuf, rsaPrivKey,RSA_PKCS1_PADDING);
+
 	printf ("Decrypted challenge: %s \n", decRecvBuf );
 
-	
-	return connection;
+	RSA_free(rsaPrivKey);
+	RSA_free(rsaPublicKey);
+	return decRecvBuf;
 }
 
+unsigned char* hashChallenge(const unsigned char* challenge ){
 
+	// SHA-1 encryption on hash
+	// sign with private key
+	// send to client
+//	unsigned char*  shaOutput = (unsigned char*) malloc(20+1); // SHA hashes to 20 bits
+	printf ("STRLEN: %lu, SizeOf: %lu \n ", strlen(challenge), sizeof challenge);
+	
+	unsigned char* shaOutput = (unsigned char*) malloc (20 +1);
+	SHA1(challenge,strlen(challenge),shaOutput);
+	return shaOutput;	
+}
+
+void sendToClient(BIO* bio, unsigned char* message ){
+	if (BIO_write(bio,message, strlen(message)) <= 0){
+		printf( "Error sending to client \n");
+		exit(1);
+	}
+}
 int main(int count, char *args[]){
 
 	char* portnum;
@@ -79,7 +117,14 @@ int main(int count, char *args[]){
 	   will wait for an incoming connection	
 	*/
 	BIO *connection = waitAndAcceptConnection(portnum);
-	connection = keyExchangeWithClient(connection);	
+	const unsigned char* challenge = getChallengeFromClient(connection);
+	unsigned char* hashedChallenge = hashChallenge(challenge);
+	sendToClient(connection,hashedChallenge);
+	BIO_free_all(connection);
+	free((unsigned char*)challenge);
+	free(hashedChallenge);	
+	
+	cleanupOpenSSL();
 	return 0;
 }
 
